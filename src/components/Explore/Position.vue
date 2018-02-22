a72eb061bca9fb24b12e674ad572e0dc7e1c4a0d
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-02-22 17:00:00 +0100

    Added position vue (not done)

---
+++ src/components/Explore/Position.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div>
+        magnitude: unknown    
+      </div>
+      <div>
+          x: , y:
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Position',
+  props: [
+      'currentMap'
+  ],
+  created() {
+      this.$openlayers.getMap(currentMap.id).addControl();
+  },
+}
+</script>
