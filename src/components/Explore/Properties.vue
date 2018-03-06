0563c66c408fa6fa8ba0b13bcd6f3655afa593bb
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-03-06 12:19:44 +0100

    Working on multilayer properties

--- src/components/Explore/Properties.vue
+++ src/components/Explore/Properties.vue
@@ -5,7 +5,7 @@
       </select>
       <select @change="handleKey" v-model="propertySelected" name="properties" id="properties">
           <option value="">No key selected</option>
-          <option v-for="property in propertiesAvailable" :key="property.key" :value="property">{{property.key}}</option>
+          <option v-for="property in propertiesToShow" :key="property.key" :value="property">{{property.key}}</option>
       </select>
   </div>
 </template>
@@ -18,11 +18,13 @@ export default {
     name: 'Properties',
     props: [
         'currentMap',
+        'layersSelected',
     ],
     data() {
         return {
             propertiesAvailable: [],
             propertySelected: "",
+            propertiesToShow: [],
             colors: [
                 {
                     value: '#ffffff',
@@ -65,32 +67,43 @@ export default {
         features() {
             let layers = this.$openlayers.getMap(this.currentMap.id).getLayers();
             let index = layers.getArray().findIndex(layer => layer.get('title') === 33)
-            return layers.getArray()[index].getSource().getFeatures();
+            return layers.getArray()[index].getSource() ? layers.getArray()[index].getSource().getFeatures() : null;
         },
     },
+    watch: {
+        layersSelected(newValue) {
+           this.propertiesToShow = newValue === [] ? [] : this.propertiesAvailable;
+        }
+    },
     methods: {
-        handleKey() {         
-            this.keys.map(key => {
-                let index = this.features.findIndex(feature => feature.getId() == key.idAnnotation);
-                let text = new Text({
-                    font: '24px sans-serif',
-                    fill: new Fill({
-                        color: this.colorSelected,
-                    }),
-                    text: key.value,
-                    overflow: true,
+        handleKey() {
+            this.removeKeys(); 
+            this.layersSelected.map(layer => {
+                api.get(`/api/user/${layer.id}/imageinstance/1577/annotationposition.json?key=${this.propertySelected.key}`).then(data => {
+                    this.keys = data.data.collection;
+                    this.keys.map(key => {
+                        let index = this.features.findIndex(feature => feature.getId() == key.idAnnotation);
+                        let text = new Text({
+                            font: '24px sans-serif',
+                            fill: new Fill({
+                                color: this.colorSelected,
+                            }),
+                            text: key.value,
+                            overflow: true,
+                        })
+                        this.features[index].getStyle().setText(text);
+                    })
+                    this.$openlayers.getMap(this.currentMap.id).render();
                 })
-                this.features[index].getStyle().setText(text);
-                this.$openlayers.getMap(this.currentMap.id).renderSync();
-            })
+            })        
         },
+        removeKeys() {
+            this.features.map(feature => feature.getStyle().setText(new Text({text: ""})));
+        }
     },
     created() {
         api.get(`/api/annotation/property/key.json?idImage=${this.currentMap.imageId}&user=true`).then(data => {
             this.propertiesAvailable = data.data.collection;
-        })
-        api.get(`/api/user/33/imageinstance/1577/annotationposition.json?key=slot`).then(data => {
-            this.keys = data.data.collection;
         })
     }
 }
