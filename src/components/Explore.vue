c1f1eaa8aee0ec4a664ccc7f88bad7586f722981
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-03-26 11:50:41 +0200

    Added selected feature details

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -21,7 +21,7 @@
         <annotation-layers @updateLayers="setUpdateLayers" @vectorLayersOpacity="setVectorLayersOpacity" @layersSelected="setLayersSelected" @userLayers="setUserLayers" :isReviewing="isReviewing" :updateLayers="updateLayers" :termsToShow="termsToShow" :showWithNoTerm="showWithNoTerm" :allTerms="allTerms" :currentMap="currentMap"></annotation-layers>
         <interactions @updateLayers="setUpdateLayers" @featureSelected="setFeatureSelected" :currentMap="currentMap" :isReviewing="isReviewing" :vectorLayersOpacity="vectorLayersOpacity"></interactions>
         <ontology :featureSelectedData="featureSelectedData" :featureSelected="featureSelected" :vectorLayersOpacity="vectorLayersOpacity" @showTerms="showTerms" @showWithNoTerm="setShowWithNoTerm" @allTerms="setAllTerms"></ontology>
-        <review v-if="isReviewing"></review>
+        <review v-if="isReviewing" :featureSelectedData="featureSelectedData" :featureSelected="featureSelected" :userLayers="userLayers"></review>
         <multidimension v-if="imageGroupIndex[0]" @imageGroupHasChanged="setImageGroup" :imageGroupIndex="imageGroupIndex" :filterUrl="filterUrl" :imsBaseUrl="imsBaseUrl" @imageHasChanged="changeImage" :currentMap="currentMap"></multidimension>
         <properties :layersSelected="layersSelected" :currentMap="currentMap"></properties>
         <annotation-details @featureSelectedData="setFeatureSelectedData" :users="userLayers" :terms="allTerms" :featureSelected="featureSelected" :currentMap="currentMap"></annotation-details>
@@ -75,7 +75,7 @@ export default {
         termsToShow: [],
         showWithNoTerm: true,
         allTerms: [],
-        featureSelected: {},
+        featureSelected: undefined,
         featureSelectedData: {},
         userLayers: [],
         layersSelected: [],
--- src/components/Explore/Review.vue
+++ src/components/Explore/Review.vue
@@ -1,10 +1,51 @@
 <template>
-  <div></div>
+  <div>
+    <section>
+      <h4>Review | Action selection</h4>
+      <dl v-if="isSelected()">
+        <dt>Annotation:</dt>
+        <dd>{{featureId}}</dd>
+        <dt>User</dt>
+        <dd>{{featureUser}}</dd>
+        <dt>Created:</dt>
+        <dd>{{featureDate}}</dd>
+        <dt>Term(s):</dt>
+        <dd></dd>
+      </dl>
+    </section>
+  </div>
 </template>
 
 <script>
+import humanDate from '../../helpers/humanDate'
+
 export default {
-    name:'Review'
+    name:'Review',
+    props: [
+      'featureSelected',
+      'featureSelectedData',
+      'userLayers'
+    ],
+    computed: {
+      featureId() {
+        return this.isSelected() ? this.featureSelectedData.id : "";
+      },
+      featureUser() {
+        return this.isSelected() ? this.displayName(this.featureSelectedData.user) : "";
+      },
+      featureDate() {
+        return this.featureSelectedData.created ? humanDate(this.featureSelectedData.created) : "";
+      },
+    },
+    methods: {
+      isSelected() {
+        return this.featureSelected != undefined;
+      },
+      displayName(userId) {
+        let index = this.userLayers.findIndex(user => user.id == userId);
+        return index < 0 ? "" : `${this.userLayers[index].lastname} ${this.userLayers[index].firstname} (${this.userLayers[index].username})`
+      },
+    }
 }
 </script>
 
