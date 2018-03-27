1719fe94315a284c9d9da0740c8426224a222ae8
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-03-27 10:28:14 +0200

    Fixed reject review not working

--- src/components/Explore/Review.vue
+++ src/components/Explore/Review.vue
@@ -48,27 +48,24 @@ export default {
         return index < 0 ? "" : `${this.userLayers[index].lastname} ${this.userLayers[index].firstname} (${this.userLayers[index].username})`
       },
       acceptReview() {
-        api.put(`/api/annotation/${this.featureId}/review.json`, {
+        let id = this.featureSelectedData.parentIdent ? this.featureSelectedData.parentIdent : this.featureId;
+        api.put(`/api/annotation/${id}/review.json`, {
           id:this.featureId,
           terms: this.featureSelectedData.term,
         }).then(data => {
           this.featureSelected.getStyle().getStroke().setColor([91, 183, 91]);
           this.featureSelected.changed();
           this.$emit('featureSelectedData', data.data.reviewedannotation);
-          // this.$emit('updateLayers', true);
         })
       },
       rejectReview() {
-        api.delete(`/api/annotation/${this.featureSelectedData.parentIdent}/review.json`).then(data => {
+        let id = this.featureSelectedData.parentIdent;
+        api.delete(`/api/annotation/${id}/review.json`).then(data => {
           this.featureSelected.getStyle().getStroke().setColor([189, 54, 47]);
           this.featureSelected.changed();
-          api.get(`/api/annotation/${this.featureId}.json`).then(data => {
+          api.get(`/api/annotation/${id}.json`).then(data => {
             this.$emit('featureSelectedData', data.data);
           })
-          // this.featureSelectedData.reviewed = false;
-          // let newFeature = data.data.reviewedannotation;
-          // newFeature.reviewed = false;
-          // this.$emit('updateLayers', true);
         })
       }
     }
