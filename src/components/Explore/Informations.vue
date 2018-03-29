058adaf29e3de1491d105b1741a95da4030ce037
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-03-29 16:07:08 +0200

    Added previous/next button

--- src/components/Explore/Informations.vue
+++ src/components/Explore/Informations.vue
@@ -23,15 +23,35 @@
           <li><a href="#">More info</a></li>
         </ul>
       </div>
+      <div>
+        <button @click="setAdjacentImage('previous')">Previous image</button>
+        <button @click="setAdjacentImage('next')">Next image</button>
+      </div>
   </div>
 </template>
 
 <script>
+import OlTile from 'ol/layer/tile';
+import Zoomify from 'ol/source/zoomify';
+import Group from 'ol/layer/group';
+
 export default {
   name: 'Informations',
   props: [
-      'currentMap'
+      'currentMap',
+      'imsBaseUrl',
+      'filterUrl',
   ],
+  data() {
+    return {
+      adjacentImage: {},
+    }
+  },
+  watch: {
+    adjacentImage(newValue) {
+      this.$emit('updateMap', newValue);
+    }
+  },
   methods: {
     reviewMode() {
       api.put(`/api/imageinstance/${this.currentMap.imageId}/review.json`, {
@@ -44,7 +64,25 @@ export default {
       api.delete(`/api/imageinstance/${this.currentMap.imageId}/review.json`).then(data => {
         this.$emit('updateMap', data.data.imageinstance);
       })
-    }
+    },
+    setAdjacentImage(position) {
+      api.get(`/api/imageinstance/${this.currentMap.imageId}/${position}.json`).then(data => {
+        this.adjacentImage = data.data;
+        this.changeImage();
+      })
+    },
+    changeImage() {
+      let layer = new OlTile({
+          source: new Zoomify({
+              url: `${this.filterUrl}${this.imsBaseUrl}image/tile?zoomify=${this.adjacentImage.fullPath}/&tileGroup={TileGroup}&z={z}&x={x}&y={y}&channels=0&layer=0&timeframe=0&mimeType=${this.adjacentImage.mime}`,
+              size: [this.adjacentImage.width, this.adjacentImage.height],
+              extent: [0, 0, this.adjacentImage.width, this.adjacentImage.height],
+          }),
+          extent: [0, 0, this.adjacentImage.width, this.adjacentImage.height],
+      })
+      
+      this.$openlayers.getMap(this.currentMap.id).setLayerGroup(new Group({layers: [layer]}));
+  },
   },
 }
 </script>
